3cc159dade2ffab344578995dacf6cd95009a82e
jaofreire <joao.gabriel18872@gmail.com>
2024-07-25 11:35:17 -0300

    InitialCommit

---
+++ BIgOneChatAuthService/BIgOneChatAuthService/UserModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BIgOneChatAuthService
+{
+    public class UserModel
+    {
+        public string? Name { get; set; }
+        public string? Nickname { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? Role { get; set; }
+
+    }
+}
