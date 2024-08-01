2d25b156c3e76159dcf403d6db240fb566214670
jaofreire <joao.gabriel18872@gmail.com>
2024-08-01 19:03:27 -0300

    resolver conflito

---
+++ BIgOneChatAuthService/Application/Services/AuthService.cs
@@ -0,0 +1,76 @@
+﻿using Application.Responses;
+using Application.Services.Interfaces;
+using Application.Utils.Auth;
+using Application.Utils.Auth.Interfaces;
+using Domain.Interfaces;
+using Domain.Models;
+
+
+namespace Application.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly ITokenRepository _tokenRepository;
+        private readonly ITokensGenerator _tokenGenerator;
+
+        public AuthService(ITokenRepository tokenRepository, ITokensGenerator tokenGenerator)
+        {
+            _tokenRepository = tokenRepository;
+            _tokenGenerator = tokenGenerator;
+        }
+
+        public async Task<APIResponse<TokenModel>> GenerateToken(UserModel model)
+        {
+            var accessToken = _tokenGenerator.GenerateToken(model);
+            var refreshToken = _tokenGenerator.GenerateRefreshToken();
+
+            var tokenModel = new TokenModel()
+            {
+                Nickname = model.Nickname,
+                Token = accessToken,
+                RefreshToken = refreshToken
+            };
+
+            await _tokenRepository.Register(tokenModel);
+
+            return new APIResponse<TokenModel>(tokenModel);
+        }
+
+        public async Task<APIResponse<TokenModel>> RefreshToken(TokenModel model)
+        {
+            var tokenModel = await _tokenRepository.GetByNickname(model.Nickname);
+
+            if (tokenModel is null)
+                return new APIResponse<TokenModel>(404, "Token is not found, try sign in again");
+
+            if (tokenModel.Token != model.Token || tokenModel.RefreshToken != model.RefreshToken)
+                return new APIResponse<TokenModel>(400, "Invalid refresh/access token");
+
+            var claimsPrincipal = _tokenGenerator.GetClaimsPrincipal(model.Token);
+
+            if(claimsPrincipal is null)
+                return new APIResponse<TokenModel>(400, "Invalid access token");
+
+            var userModel = new UserModel()
+            {
+                Nickname = claimsPrincipal.Identity.Name,
+                Role = claimsPrincipal.Claims.ToList()[1].Value
+            };
+
+            var newAccessToken = _tokenGenerator.GenerateToken(userModel);
+            var newRefreshToken = _tokenGenerator.GenerateRefreshToken();
+
+            return new APIResponse<TokenModel>(new TokenModel()
+            {
+                Nickname = userModel.Nickname,
+                Token = newAccessToken,
+                RefreshToken = newRefreshToken
+            });
+
+        }
+
+
+        
+
+    }
+}
---
+++ BIgOneChatAuthService/Test/Auth.UnitTest/Data/Helper/Database/DatabaseUtils.cs
@@ -0,0 +1,28 @@
+﻿using Data.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Auth.UnitTest.Data.Helper.Database
+{
+    public static class DatabaseUtils
+    {
+        public static DatabaseContext CreateDbContext(string dbName)
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(databaseName: dbName)
+                .Options;
+
+            var dbContext = new DatabaseContext(options);
+
+            dbContext.Database.EnsureCreated();
+
+            return dbContext;
+        }
+
+        public static void ClearDatabase(DatabaseContext dbContext)
+        {
+            dbContext.RemoveRange(dbContext.Tokens);
+            dbContext.SaveChanges();
+        }
+    }
+}
