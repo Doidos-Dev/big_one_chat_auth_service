77a734910541f41237cb580f1e76d842006ca97b
jaofreire <joao.gabriel18872@gmail.com>
2024-08-02 12:47:13 -0300

    create AuthService Unit Tests. ClaimsFixture and JWTTokenFixture

--- BIgOneChatAuthService/Test/Auth.UnitTest/Application/AuthServiceTests.cs
+++ BIgOneChatAuthService/Test/Auth.UnitTest/Application/AuthServiceTests.cs
@@ -1,20 +1,22 @@
 ﻿using Application.Services;
-using Data.Persistence;
-using Auth.UnitTest.Data.Helper.Database;
 using Domain.Models;
 using Moq;
 using Domain.Interfaces;
 using Application.Utils.Auth.Interfaces;
+using Auth.UnitTest.Application.Fixture;
+using System.Security.Claims;
 
 
 namespace AuthUnitTest.Application
 {
-    public class AuthServiceTests : IDisposable
+    public class AuthServiceTests
     {
-        private static readonly DatabaseContext _context = DatabaseUtils.CreateDbContext("AuthTestDb");
         private static readonly Mock<ITokenRepository> _tokenRepositoryMock = new Mock<ITokenRepository>();
         private static readonly Mock<ITokensGenerator> _tokenGeneratorMock = new Mock<ITokensGenerator>();
+        private static string _secret = "3c8727e019a42b444667a587b6001251becadabbb36bfed8087a92c18882d111";
         private UserModel _userModel;
+        private TokenModel _tokenModel;
+
         public AuthServiceTests()
         {
             _userModel = new UserModel()
@@ -23,11 +25,12 @@ namespace AuthUnitTest.Application
                 Role = "TestRole"
             };
 
-        }
-
-        public void Dispose()
-        {
-            DatabaseUtils.ClearDatabase(_context);
+            _tokenModel = new TokenModel()
+            {
+                Nickname = "TestNickname",
+                Token = "TestToken",
+                RefreshToken = "TestRefreshToken",
+            };
         }
 
         [Fact]
@@ -35,8 +38,8 @@ namespace AuthUnitTest.Application
         {
             //Arrange
             _tokenGeneratorMock.Setup(x => x.GenerateToken(_userModel))
-                .Returns("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c");
-            _tokenGeneratorMock.Setup(x => x.GenerateRefreshToken()).Returns("238128jkj1k31m2k3km12km");
+                .Returns("TestToken");
+            _tokenGeneratorMock.Setup(x => x.GenerateRefreshToken()).Returns("TestRefreshToken");
             var authService = new AuthService(_tokenRepositoryMock.Object, _tokenGeneratorMock.Object);
 
             //Act
@@ -52,7 +55,8 @@ namespace AuthUnitTest.Application
             //Arrange
             _tokenGeneratorMock.Setup(x => x.GenerateToken(_userModel))
                 .Returns("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c");
-            _tokenGeneratorMock.Setup(x => x.GenerateRefreshToken()).Returns("238128jkj1k31m2k3km12km");
+            _tokenGeneratorMock.Setup(x => x.GenerateRefreshToken()).
+                Returns("238128jkj1k31m2k3km12km");
             var authService = new AuthService(_tokenRepositoryMock.Object, _tokenGeneratorMock.Object);
 
             //Act
@@ -62,6 +66,110 @@ namespace AuthUnitTest.Application
             Assert.NotNull(result.Response.Token);
             Assert.NotNull(result.Response.RefreshToken);
         }
+
+        [Fact]
+        public async void Refresh_Token_Success()
+        {
+            //Arrange
+
+            var oldTokenModel = new TokenModel()
+            {
+                Nickname = "TestNickname",
+                Token = JWTTokenFixture.GenerateToken(_userModel, _secret),
+                RefreshToken = JWTTokenFixture.GenerateRefreshToken()
+            };
+            var newTokenModel = new TokenModel()
+            {
+                Nickname = _userModel.Nickname,
+                Token = "NewToken",
+                RefreshToken = "NewRefreshToken"
+            };
+            _tokenRepositoryMock.Setup(x => x.GetByNickname(oldTokenModel.Nickname)).Returns(Task.FromResult(oldTokenModel));
+            _tokenGeneratorMock.Setup(x => x.GetClaimsPrincipal(oldTokenModel.Token)).Returns(ClaimsFixture.GenerateClaimsPrincipal(oldTokenModel.Token, _secret));
+            _tokenGeneratorMock.Setup(x => x.GenerateToken(_userModel)).Returns(newTokenModel.Token);
+            _tokenGeneratorMock.Setup(x => x.GenerateRefreshToken()).Returns(newTokenModel.RefreshToken);
+            var authService = new AuthService(_tokenRepositoryMock.Object, _tokenGeneratorMock.Object);
+
+            //act
+            var result = await authService.RefreshToken(oldTokenModel);
+
+            //Assert
+            Assert.NotEqual(result.Response.Token, _tokenModel.Token);
+            Assert.NotEqual(result.Response.RefreshToken, _tokenModel.RefreshToken);
+            
+        }
+
+        [Fact]
+        public async void Refresh_Token_Old_Tolken_Is_Not_Found_Fail()
+        {
+            //Arrange
+            var oldTokenModel = new TokenModel()
+            {
+                Nickname = "TestNickname"
+            };
+            _tokenRepositoryMock.Setup(x => x.GetByNickname(oldTokenModel.Nickname)).Returns(Task.FromResult((TokenModel)null));
+            var authService = new AuthService(_tokenRepositoryMock.Object, _tokenGeneratorMock.Object);
+
+            //Act
+            var result = await authService.RefreshToken(oldTokenModel);
+
+            //Assert
+            Assert.Equal(result.CodeResponse, 404);
+            Assert.NotNull(result.Message);
+
+        }
+
+        [Fact]
+        public async void Refresh_Token_Invalid_Access_Or_Refresh_Token_Fail()
+        {
+            //Arrange
+            var requestTokenModel = new TokenModel()
+            {
+                Nickname = "TestNickname",
+                Token = JWTTokenFixture.GenerateToken(_userModel, _secret),
+                RefreshToken = JWTTokenFixture.GenerateRefreshToken()
+            };
+            var oldTokenModelFetched = new TokenModel()
+            {
+                Nickname = "TestNickname",
+                Token = JWTTokenFixture.GenerateToken(_userModel, _secret),
+                RefreshToken = JWTTokenFixture.GenerateRefreshToken()
+            };
+            _tokenRepositoryMock.Setup(x => x.GetByNickname(requestTokenModel.Nickname)).Returns(Task.FromResult(oldTokenModelFetched));
+            var authService = new AuthService(_tokenRepositoryMock.Object, _tokenGeneratorMock.Object);
+
+            //Act
+            var result = await authService.RefreshToken(requestTokenModel);
+
+            //Assert
+            Assert.Equal(result.CodeResponse, 400);
+            Assert.NotNull(result.Message);
+
+        }
+
+        [Fact]
+        public async void Refresh_Token_Invalid_Token_Claims_Principal_Return_Null_Fail()
+        {
+            //Arrange
+            var requestTokenModel = new TokenModel()
+            {
+                Nickname = "TestNickname",
+                Token = JWTTokenFixture.GenerateToken(_userModel, _secret),
+                RefreshToken = JWTTokenFixture.GenerateRefreshToken()
+            };
+
+            _tokenRepositoryMock.Setup(x => x.GetByNickname(requestTokenModel.Nickname)).Returns(Task.FromResult(requestTokenModel));
+            _tokenGeneratorMock.Setup(x => x.GetClaimsPrincipal(requestTokenModel.Token)).Returns((ClaimsPrincipal)null);
+            var authService = new AuthService(_tokenRepositoryMock.Object, _tokenGeneratorMock.Object);
+
+            //Act
+            var result = await authService.RefreshToken(requestTokenModel);
+
+            //Assert
+            Assert.Equal(result.CodeResponse, 400);
+            Assert.NotNull(result.Message);
+        }
+
 
     }
 }
---
+++ BIgOneChatAuthService/Test/Auth.UnitTest/Application/Fixture/ClaimsFixture.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auth.UnitTest.Application.Fixture
+{
+    public static class ClaimsFixture
+    {
+        
+        public static ClaimsPrincipal GenerateClaimsPrincipal(string accessToken, string secret)
+        {
+            byte[] key = Encoding.ASCII.GetBytes(secret);
+
+            var validationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateAudience = false,
+                ValidateIssuer = true,
+                ValidIssuer = "https://test-issuer-domain/"
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var principal = tokenHandler.ValidateToken(accessToken, validationParameters, out SecurityToken validatedToken);
+            
+            return principal;
+        }
+    }
+}
